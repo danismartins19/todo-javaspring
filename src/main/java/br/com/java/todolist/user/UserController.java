9e483a2ad4d0c893bbbc558f1f81d040bb4d11dd
Daniel <danidiamoo1@gmail.com>
2023-10-09 13:20:26 -0300

    project setup and user controller created

---
+++ src/main/java/br/com/java/todolist/user/UserController.java
@@ -0,0 +1,16 @@
+package br.com.java.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  
+  @PostMapping("/")
+  public UserModel create(@RequestBody UserModel userModel){
+    return userModel;
+  }
+}
