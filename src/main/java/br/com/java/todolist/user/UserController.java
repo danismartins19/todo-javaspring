b7c7bfa29dfc8587ab461368b1d1364ec550ae74
Daniel <danidiamoo1@gmail.com>
2023-10-10 17:59:39 -0300

    Database and repository created

--- src/main/java/br/com/java/todolist/user/UserController.java
+++ src/main/java/br/com/java/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.java.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
   
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
   public UserModel create(@RequestBody UserModel userModel){
-    return userModel;
-  }
+    var userCreated = this.userRepository.save(userModel);
+    return userCreated;
+  } 
 }
--- src/main/java/br/com/java/todolist/user/UserModel.java
+++ src/main/java/br/com/java/todolist/user/UserModel.java
@@ -1,7 +1,32 @@
 package br.com.java.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Access;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Data;
+
+@Data
+@Entity(name = "usuarios")
 public class UserModel {
+  
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+  
+  @Column(unique = true)
   public String username;
   public String name;
   public String password;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
