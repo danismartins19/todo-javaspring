7b622324e76899c64cf103df8e2395ce8dca3b7e
Daniel <danidiamoo1@gmail.com>
2023-10-11 10:30:03 -0300

    Responses and sign up validation created

--- src/main/java/br/com/java/todolist/user/UserModel.java
+++ src/main/java/br/com/java/todolist/user/UserModel.java
@@ -5,13 +5,10 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.Access;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 
 @Data
