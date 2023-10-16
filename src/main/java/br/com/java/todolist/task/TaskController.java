85607bf4db9b169a6f0b16ec8e06b05885f79b6c
Daniel Martins <danidiamoo1@gmail.com>
2023-10-16 20:06:16 -0300

    exception handler created

--- src/main/java/br/com/java/todolist/task/TaskController.java
+++ src/main/java/br/com/java/todolist/task/TaskController.java
@@ -57,11 +57,24 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update (@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
+  public ResponseEntity update (@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
 
     var task = this.taskRepository.findById(id).orElse(null);
+    //verifica se a tarefa existe
+    if(task == null){
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarefa não encontrada");
+    }
+
+    //verifica se o usuario autenticado é o criador da task
+    var idUser = request.getAttribute("idUser");
+    if(!task.getIdUser().equals(idUser)){
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario não tem permissão para alterar essa tarefa!");
+    }
     Utils.copyNonNullProperties(taskModel, task);
-    return this.taskRepository.save(task);
+
+    //atualizando a task no banco
+    var taskSave = this.taskRepository.save(task);
+    return ResponseEntity.ok().body(taskSave);
   }
 
 }
